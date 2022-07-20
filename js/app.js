c1835f25b0b6c137e74be7f93e4977eb56b9d3c6
MohammadAslani76 <mohammadaslani001@gmail.com>
2022-07-20 09:55:58 +0430

    sass landing page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,53 @@
 const openMenu = document.querySelector("#openMenu");
 const closeMenu = document.querySelector("#closeMenu");
 const menu = document.querySelector("#menu");
-openMenu.addEventListener("click" , () => {
-    menu.style.display= 'block';
-    menu.classList.add('show');
-    menu.classList.remove('hide');
+const menuBackground = document.querySelector('.menu__background');
+const body = document.querySelector('body');
+const h2 = document.querySelector('#h2');
+openMenu.addEventListener("click" , (event) => {
+    event.stopPropagation();
+    menu.classList.toggle('show');
+    menuBackground.style.display = 'block';
 })
 closeMenu.addEventListener("click" , () => {
-    menu.classList.add('hide');
-    menu.classList.remove('show');
-    menu.style.display= "none";
+    menu.classList.toggle('show');
+    menuBackground.style.display = 'none';
 })
+menu.addEventListener('click', (event) => {
+    event.stopPropagation();
+})
+if (window.innerWidth < 768){
+    h2.innerHTML = "Our work process make your dream true";
+}
+body.addEventListener('click' , () => {
+    if (menu.classList.contains('show')){
+        menu.classList.remove('show');
+        menuBackground.style.display = 'none';
+    }
+})
+//Swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        "@0.75": {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        "@1.00": {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+    },navigation: {
+        nextEl: '.btnComments__next',
+        prevEl: '.btnComments__prev',
+    }
+});
